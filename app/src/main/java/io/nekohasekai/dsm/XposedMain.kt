2fe92fa59782e624496e91598547212bf0ff38df
Kira Roubin <kirao@duck.com>
2024-02-03 00:24:01 +0300

    fix: Catching sponsored messages

--- app/src/main/java/io/nekohasekai/dsm/XposedMain.kt
+++ app/src/main/java/io/nekohasekai/dsm/XposedMain.kt
@@ -9,26 +9,25 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
 class XposedMain : IXposedHookLoadPackage {
 
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        if (lpparam.packageName != "org.thunderdog.challegram") {
-            return
-        }
-        runCatching {
-            XposedHelpers.findAndHookMethod(
-                Class.forName(
-                    "org.drinkless.td.libcore.telegram.TdApi\$GetChatSponsoredMessage",
-                    false,
-                    lpparam.classLoader
-                ),
-                "getConstructor",
-                object : XC_MethodReplacement() {
-                    override fun replaceHookedMethod(param: MethodHookParam?): Any {
-                        // getMe
-                        return -191516033
+        if (lpparam.packageName == "org.thunderdog.challegram" || lpparam.packageName == "moe.kirao.mgx" || lpparam.packageName == "com.tgxalpha" || lpparam.packageName == "com.jasonkhew96.pigeongramx") {
+            runCatching {
+                XposedHelpers.findAndHookMethod(
+                    Class.forName(
+                        "org.drinkless.td.libcore.telegram.TdApi\$GetChatSponsoredMessages",
+                        false,
+                        lpparam.classLoader
+                    ),
+                    "getConstructor",
+                    object : XC_MethodReplacement() {
+                        override fun replaceHookedMethod(param: MethodHookParam?): Any {
+                            // getMe
+                            return -191516033
+                        }
                     }
-                }
-            )
-        }.onFailure {
-            Log.e("DisableSponsoredMessage", "hook failed", it)
+                )
+            }.onFailure {
+                Log.e("DisableSponsoredMessage", "hook failed", it)
+            }
         }
     }
 }
